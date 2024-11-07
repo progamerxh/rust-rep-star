135d18120d7c40873448d5311202c338daca7ffd
Hiep Dinh <xuanhiep28697@gmail.com>
2024-11-07 10:48:22 +0700

    ✨ Add SQLx support with PostgreSQL connection pool and query example

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use sqlx::postgres::PgPoolOptions;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -11,6 +12,25 @@ async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send +
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
+
+    // Create a connection pool
+    //  for MySQL/MariaDB, use MySqlPoolOptions::new()
+    //  for SQLite, use SqlitePoolOptions::new()
+    //  etc.
+    let pool = PgPoolOptions::new()
+        .max_connections(1)
+        .connect("postgresql://postgres:postgres@127.0.0.1/rep-star")
+        .await
+        .map_err(|e| shuttle_runtime::Error::from(anyhow::Error::new(e)))?;
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await
+        .map_err(|e| shuttle_runtime::Error::from(anyhow::Error::new(e)))?;
+
+    assert_eq!(row.0, 150);
 
     Ok(config.into())
 }
