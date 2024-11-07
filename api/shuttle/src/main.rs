3db8efb815f6ed0c5c186600becf6e76cb9186a4
Hiep Dinh <xuanhiep28697@gmail.com>
2024-11-07 11:32:02 +0700

    ✨ Add version endpoint and integrate tracing for database queries

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -2,33 +2,49 @@ use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::Executor;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/version")]
+async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting version");
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(db.get_ref())
+        .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
+}
+
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
-    };
-
     // Create a connection pool
     //  for MySQL/MariaDB, use MySqlPoolOptions::new()
     //  for SQLite, use SqlitePoolOptions::new()
     //  etc.
     let pool = PgPoolOptions::new()
         .max_connections(1)
-        .connect("postgresql://postgres:postgres@127.0.0.1/rep-star")
+        .connect("postgresql://tsdbadmin:u1ttq9i4o6ex24db@bhrhgjuo9r.m19kjwh83w.tsdb.cloud.timescale.com:39098/tsdb")
         .await
         .map_err(CustomError::new)?;
 
+    let pool = actix_web::web::Data::new(pool);
+
+    tracing::info!("Database connection pool created successfully.");
+
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.app_data(pool).service(hello_world).service(version);
+    };
+
     // initialize the database if not already initialized
-    pool.execute(include_str!("../../db/schema.sql"))
-        .await
-        .map_err(CustomError::new)?;
+    // pool.execute(include_str!("../../db/schema.sql"))
+    //     .await
+    //     .map_err(CustomError::new)?;
 
     Ok(config.into())
 }
